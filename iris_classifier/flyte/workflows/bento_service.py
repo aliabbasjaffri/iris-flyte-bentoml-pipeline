0ac49e8acb4f611553fa8a19e79068e3a4a2fa21
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2022-03-15 00:53:12 +0100

    Updated bentoml service for prediction target name

--- iris_classifier/flyte/workflows/bento_service.py
+++ iris_classifier/flyte/workflows/bento_service.py
@@ -1,3 +1,7 @@
+try:
+    from datasource import load_iris_dataset
+except ImportError:
+    from .datasource import load_iris_dataset
 import torch
 import bentoml
 import numpy as np
@@ -5,12 +9,14 @@ from bentoml.io import NumpyNdarray
 from torch.autograd import Variable
 
 artifact_name = "iris_classifier"
-
+_, _, target_names, _ = load_iris_dataset()
 iris_classifier_runner = bentoml.pytorch.load_runner(tag=artifact_name)
 svc = bentoml.Service(name=artifact_name, runners=[iris_classifier_runner])
 
 
 @svc.api(input=NumpyNdarray(), output=NumpyNdarray())
-def classify(input_series: np.ndarray) -> np.ndarray:
+def classify(input_series: np.ndarray) -> str:
     x = Variable(torch.FloatTensor(input_series))
-    return iris_classifier_runner.run(x)
+    prediction = iris_classifier_runner.run(x)
+    value = target_names[np.where(prediction == 1.0)[0]]
+    return value
